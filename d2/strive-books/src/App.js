f18e681836032b58596b2deccc22ee3149eee729
BertoPolo <bertopolo91@gmail.com>
2022-02-14 13:04:52 +0100

    ç

--- d2/strive-books/src/App.js
+++ d2/strive-books/src/App.js
@@ -16,3 +16,11 @@ function App() {
 }
 
 export default App;
+
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/"),
+  {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjA1MTBjOWRhNDBjOTAwMTVmYzhkNmEiLCJpYXQiOjE2NDQ0OTkxNDUsImV4cCI6MTY0NTcwODc0NX0.8h2PlkIIIJAqbPtyBA0tLWSW3AOgUbUE1WX58kR_UK4",
+    },
+  };
