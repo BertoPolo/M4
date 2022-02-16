dbe6a1048424b802e6ae32f6f4d3e1b57d740a27
BertoPolo <bertopolo91@gmail.com>
2022-02-16 16:37:28 +0100

    until 4

--- d3/d3-hw/src/App.js
+++ d3/d3-hw/src/App.js
@@ -4,6 +4,7 @@ import WarningSign from "./components/WarningSign";
 import MyBadge from "./components/MyBadge";
 import SingleBook from "./components/SingleBook";
 import scifiBook from "./scifi.json";
+import BookList from "./components/BookList";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <WarningSign message="hello im da king" />
         <MyBadge text="haha! maybe in your dreams" color="secondary" />
         <SingleBook book={scifiBook[0]} />
+        <BookList books={scifiBook} />
       </header>
     </div>
   );
--- d3/d3-hw/src/components/BookList.jsx
+++ d3/d3-hw/src/components/BookList.jsx
@@ -1,2 +1,17 @@
-const BookList = () => {};
+import SingleBook from "./SingleBook";
+import { Col, Container, Row } from "react-bootstrap";
+
+const BookList = ({ books }) => {
+  return (
+    <Container>
+      <Row>
+        {books.map((oneBook) => (
+          <Col>
+            <SingleBook book={oneBook} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 export default BookList;
